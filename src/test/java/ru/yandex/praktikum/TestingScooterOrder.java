43e84d998c61342f79062f491e5f922a6c0f7eeb
Firuz Aminov <firuzaminoff@vk.com>
2023-01-30 18:31:08 +0300

    Запараметризировал форму клиента (пока без кнопок)

--- src/test/java/ru/yandex/praktikum/TestingScooterOrder.java
+++ src/test/java/ru/yandex/praktikum/TestingScooterOrder.java
@@ -4,19 +4,41 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.yandex.prakrikum.HeaderWithTwoButtons;
 import ru.yandex.prakrikum.MainPageScooter;
 import ru.yandex.prakrikum.RentInformation;
 import ru.yandex.prakrikum.UserInformationForOrder;
-
+@RunWith(Parameterized.class)
 public class TestingScooterOrder {
     private WebDriver driver;
 
-    public TestingScooterOrder() {
-
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String phoneNumber;
+    private final String date;
+    private final String comment;
+    public TestingScooterOrder(String name, String surname, String address, String phoneNumber, String date, String comment) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.date = date;
+        this.comment = comment;
     }
+
+    @Parameterized.Parameters // добавили аннотацию
+    public static Object[][] inputData() {
+        return new Object[][]{
+                {"Фируз", "Аминов", "ул. Московская, д. 23, кв. 12","+75456783454","25.01.2023","Люблю кататься на самокате"},
+                {"Дмитрий","Левый", "ул. Пушкина, д. 666, кв. 13","+76666666666", "12.02.2023", "Я очень люблю кататься на самокате"},
+        };
+    }
+
 
     @Before
     public void setUp() {
@@ -30,43 +52,15 @@ public class TestingScooterOrder {
         HeaderWithTwoButtons headerWithTwoButtons = new HeaderWithTwoButtons(driver);
         headerWithTwoButtons.clickToOrderButton();
         UserInformationForOrder userInformationForOrder = new UserInformationForOrder(driver);
-        String nameFirstDataSet = "Фируз";
-        String surnameFirstDataSet = "Аминов";
-        String addressFirstDataSet = "ул. Московская, д. 23, кв. 12";
-        String phoneFirstDataSet = "+75456783454";
-        userInformationForOrder.inputInformationAboutCustomerFirstDataSet(nameFirstDataSet, surnameFirstDataSet, addressFirstDataSet, phoneFirstDataSet);
+        userInformationForOrder.inputInformationAboutCustomerFirstDataSet(name, surname, address, phoneNumber);
         userInformationForOrder.clickNextButton();
         RentInformation rentInformation = new RentInformation(driver);
-        String dateFirstDataSet = "25.01.2023";
-        String commentFirstDataSet = "Люблю кататься на самокате";
-        rentInformation.inputInformationForRentFirstDataSet(dateFirstDataSet, commentFirstDataSet);
+        rentInformation.inputInformationForRentFirstDataSet(date, comment);
         rentInformation.clickOrderButton();
         Assert.assertTrue(rentInformation.isDisplayedInformationSure());
         rentInformation.clickApproveButtonYes();
         Assert.assertTrue(rentInformation.isDisplayedSuccessWindow());
 
-
-    }
-
-    @Test
-    public void makeScooterOrderWithDownButton() {
-        MainPageScooter headerWithTwoButtonsDown = new MainPageScooter(driver);
-        headerWithTwoButtonsDown.clickToOrderDownButton();
-        UserInformationForOrder userInFormationForOrderDown = new UserInformationForOrder(driver);
-        String nameSecondDataSet = "Дмитрий";
-        String surnameSecondDataSet = "Левый";
-        String addressSecondDataSet = "ул. Пушкина, д. 666, кв. 13";
-        String phoneSecondDataSet = "+76666666666";
-        userInFormationForOrderDown.inputInformationAboutCustomerSecondDataSet(nameSecondDataSet, surnameSecondDataSet, addressSecondDataSet, phoneSecondDataSet);
-        userInFormationForOrderDown.clickNextButton();
-        RentInformation rentInformation = new RentInformation(driver);
-        String dateSecondDataSet = "01.01.2023";
-        String commentSecondDataSet = "Я очень люблю кататься на самокате";
-        rentInformation.inputInformationForRentSecondDataSet(dateSecondDataSet, commentSecondDataSet);
-        rentInformation.clickOrderButton();
-        Assert.assertTrue(rentInformation.isDisplayedInformationSure());
-        rentInformation.clickApproveButtonYes();
-        Assert.assertTrue(rentInformation.isDisplayedSuccessWindow());
     }
 
     @After
